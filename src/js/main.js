aef0ba3db1018c915e2860d926fc5320f954c083
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2022-01-24 12:44:05 +0300

    Реализация видеоплеера, Подключение API Youtube

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import Slider from "./modules/slider.js";
+import VideoPlayer from './modules/videoPlayer.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
+    const player = new VideoPlayer('.play', '.overlay');
     slider.render();
+    player.init();
 });
